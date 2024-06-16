a5b46fc3259a10f03d4ea7a35d537ee4c13807bb
VVervolk <vervolk1234@gmail.com>
2024-06-16 18:41:02 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import posthtml from "posthtml";
+import include from "posthtml-include";
+
+// Функция для использования posthtml-include
+const posthtmlPlugin = () => {
+  return {
+    name: "vite-plugin-posthtml-include",
+    transformIndexHtml: async (html) => {
+      const result = await posthtml([include({ root: "./src" })]).process(html);
+      return result.html;
+    },
+  };
+};
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist", // Каталог сборки будет создан на уровень выше src
+  },
+  server: {
+    open: true, // Автоматически открывать браузер при запуске сервера
+  },
+  plugins: [createHtmlPlugin(), posthtmlPlugin()],
+});
