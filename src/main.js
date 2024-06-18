4bdbc08c439a8d0a83975c4ce00527f81e54cae4
VVervolk <vervolk1234@gmail.com>
2024-06-18 17:28:50 +0300

    complete hero section and about section

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import "./style.css";
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Находим кнопку по её ID
+  const openButton = document.querySelector(".header__button-menu");
+  const closeButton = document.querySelector(".mobile-menu__close-icon");
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-document.querySelector("#app").innerHTML = `
-  
-`;
-
-setupCounter(document.querySelector("#counter"));
+  openButton.addEventListener("click", () => {
+    mobileMenu.classList.add("is-open");
+  });
+  closeButton.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+  });
+});
