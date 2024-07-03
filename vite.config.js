9ff0c417de850b612ca695cee79c35e6b86af389
VVervolk <vervolk1234@gmail.com>
2024-07-03 21:19:48 +0300

    try 6

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ const posthtmlPlugin = () => {
 export default defineConfig({
   root: "src",
   build: {
-    outDir: "dist", // Каталог сборки будет создан на уровень выше src
+    outDir: "../dist", // Каталог сборки будет создан на уровень выше src
   },
   server: {
     open: true, // Автоматически открывать браузер при запуске сервера
