e594d4f0f25c6af6aa7e9b49f5c4bdbc732e33db
VVervolk <vervolk1234@gmail.com>
2024-07-03 18:33:47 +0300

    fix routes for vercel 4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import posthtml from "posthtml";
 import include from "posthtml-include";
-
+import { viteStaticCopy } from "vite-plugin-static-copy";
 // Функция для использования posthtml-include
 const posthtmlPlugin = () => {
   return {
@@ -17,10 +17,21 @@ const posthtmlPlugin = () => {
 export default defineConfig({
   root: "src",
   build: {
-    outDir: "../dist", // Каталог сборки будет создан на уровень выше src
+    outDir: "dist", // Каталог сборки будет создан на уровень выше src
   },
   server: {
     open: true, // Автоматически открывать браузер при запуске сервера
   },
-  plugins: [createHtmlPlugin(), posthtmlPlugin()],
+  plugins: [
+    createHtmlPlugin(),
+    posthtmlPlugin(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "images",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
 });
