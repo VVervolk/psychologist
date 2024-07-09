0ffa47ce165a81dc51843c8fe8e86904e950eea7
VVervolk <vervolk1234@gmail.com>
2024-07-09 15:58:31 +0300

    add footer

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // Находим кнопку по её ID
   const openButton = document.querySelector(".header__button-menu");
   const closeButton = document.querySelector(".mobile-menu__close-icon");
+  const nav = document.querySelector(".mobile-menu__nav");
   const mobileMenu = document.querySelector(".mobile-menu");
 
   openButton.addEventListener("click", () => {
     mobileMenu.classList.add("is-open");
   });
   closeButton.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+  });
+  nav.addEventListener("click", () => {
     mobileMenu.classList.remove("is-open");
   });
 });
